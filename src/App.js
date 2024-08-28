464b5f5a4fe0f9fa612d919795883cf7c445f904
Lucas Vilas <93259672+lucasvxl@users.noreply.github.com>
2024-08-28 11:35:43 +0000

    Att

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import ListaProdutos from './components/ListaProdutos';
+import axios from 'axios';
+import { useEffect, useState } from 'react';  
+
+function App() {
+  const [produtos, setProdutos] = useState([]);
+ 
+  useEffect(()=>{
+    carregaProdutos();
+  }, []); 
+
+  function carregaProdutos() {
+    axios.get('https://app-api-tapwm.onrender.com/api/produtos')
+    .then(res=>{
+      setProdutos( res.data ); 
+     //console.log(produtos); 
+    }); 
+  } 
+
+  return (
+    <div>
+      <h1>Lista Produtos</h1>
+      <ListaProdutos produtos={produtos}/> 
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/components/ListaProdutos/index.js
@@ -0,0 +1,15 @@
+import Produto from "../Produto";
+
+
+const ListaProdutos = ({ produtos }) => {
+    return (
+        <div> {produtos.map(
+                (p) => (<Produto valor={p} ></Produto>)
+            )}  
+        </div> 
+    );
+};
+
+export default ListaProdutos; 
+
+
